3ec5fcada42cdbffba3e1fd356443aac1c6a7b85
Murodjon Yusupov Jamoldin o'g'li <100166733+MurodjonYusupov@users.noreply.github.com>
2023-07-25 15:27:33 +0500

    14-dars bajarildi

---
+++ 14-dars/lug'at.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 25 12:44:40 2023
+
+@author: MurodjonUser
+"""
+
+# otam = {
+#         "fish":"bekiyev jamoldin yusupovich",
+#         "yosh":64,
+#         "tug_viloyat":"andijon",
+#         "kasb":"usta",
+#         "malumoti":"o'rta maxsus"
+#         }
+# print(f"Otam {otam['fish'].title()}, {2023-otam['yosh']}-yilda, {otam['tug_viloyat'].title()} viloyatida tug'ilgan!")
+
+# jigarlar = {
+#     "dadam":"o'rama",
+#     "opam":"shirin guruch",
+#     "akam":"dangasa somsa",
+#     "ejelerim":"shirinlik"
+#     }
+# print(f"Dadamning sevimli taomi: {jigarlar['dadam'].upper()}")
+# print(f"Akamning sevimli taomi: {jigarlar['akam'].upper()}")
+# print(f"Opamning sevimli taomi: {jigarlar['opam'].upper()}")
+
+# py_dic = {
+#     "int":"butun sonlar tipi",
+#     "float":"o'nlik sonlar tipi",
+#     "if":"agar, shart operatori",
+#     "else":"if - operatori yolg'on qiymat olsa bajariluvchi operator"
+#     }
+
+# lugat = {
+#     "apple":"olma",
+#     "green":"yashil",
+#     "parrot":"to'tiqush",
+#     "banana":"banan"
+#     }
+# soz = input("Biron so'zni kiriting va men tarjima qilaman: >>>")
+
+# if soz in lugat:
+#     print(f"{lugat[soz]}")
+# else:
+#     print( "Bunday so'z mavjud emas")
