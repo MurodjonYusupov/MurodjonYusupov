c08353a047def600fbf54be77d4f1803caba4b61
Murodjon Yusupov Jamoldin o'g'li <100166733+MurodjonYusupov@users.noreply.github.com>
2023-08-14 15:24:04 +0500

    Create untitled0.py

---
+++ amaliyot/untitled0.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 12 12:39:48 2023
+
+@author: MurodjonUser
+"""
+# import os
+# g = 'salom'
+# print(g)
+
+# os.system("cls")
+
+
+def salom():
+    son = 1
+    try:
+        if son == 1:
+            if son == 1:
+                5 > 'c'
+            # return 5
+        else:
+            return 7
+    except TypeError:
+            print("asss")
+    return 10
+
+print(salom())
